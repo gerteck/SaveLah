238d0acb17634b039e859cd5669a16e398305dee
CJerrong <chong.jer.rong@gmail.com>
2023-05-28 14:12:24 +0800

    Remove backend to be placed in another repo

---
+++ src/assets/common/baseUrl.js
@@ -0,0 +1,11 @@
+import { defineAnimation } from "react-native-reanimated";
+import { Platform } from "react-native-web";
+
+let baseURL = '';
+
+{Platform.OS == 'android'
+? baseURL = 'http://10.0.2.2:3000/api/v1/'
+: baseURL = 'http://localhost:3000/api/v1/'
+}
+
+export default baseURL;

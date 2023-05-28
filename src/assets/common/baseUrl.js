590ff018f54fbb443ad9d70940ae450ab55aba40
CJerrong <chong.jer.rong@gmail.com>
2023-05-28 15:03:34 +0800

    Edit baseURL in common assets for deployment on render

--- src/assets/common/baseUrl.js
+++ src/assets/common/baseUrl.js
@@ -1,11 +1,13 @@
 import { defineAnimation } from "react-native-reanimated";
 import { Platform } from "react-native-web";
 
-let baseURL = '';
+let baseURL = 'https://savelah-server.onrender.com/api/v1/';
 
-{Platform.OS == 'android'
-? baseURL = 'http://10.0.2.2:3000/api/v1/'
-: baseURL = 'http://localhost:3000/api/v1/'
-}
+// let baseURL = '';
+
+// {Platform.OS == 'android'
+// ? baseURL = 'http://10.0.2.2:3000/api/v1/'
+// : baseURL = 'http://localhost:3000/api/v1/'
+// }
 
 export default baseURL;

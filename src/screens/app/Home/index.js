8230ff0aa7fb74b7025e2d1c07e341893c126b1c
CJerrong <chong.jer.rong@gmail.com>
2023-06-21 01:46:03 +0800

    Add functionality in the home screen to toggle between week and month

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -13,6 +13,8 @@ import { useEffect } from "react";
 
 import { getApp } from "firebase/app";
 import TopSpendingTabs from "../../../components/TopSpendingTabs";
+import { BarChart } from "react-native-chart-kit";
+import { colors } from "../../../utils/colors";
 
 
 const Home = ( { navigation } ) => {
@@ -23,7 +25,13 @@ const Home = ( { navigation } ) => {
     // logout quickfix can fix later
 
     const [ expense, setExpense ] = useState('0');
+    const [ categories, setCategories ] = useState([]);
+    const [ expenseLastMonth, setExpenseLastMonth ] = useState('0');
     const [ error, setError ] = useState(null);
+
+    const [ expenseWeek, setExpenseWeek ] = useState('0');
+    const [ categoriesWeek, setCategoriesWeek ] = useState([]);
+    const [ expenseLastWeek, setExpenseLastWeek ] = useState('0');
 
     // // Refresh on User Change
     useEffect(() => {
@@ -40,8 +48,6 @@ const Home = ( { navigation } ) => {
         const docSnap = await getDoc(userProfileRef);
         return docSnap.data();
     }
-
-    const [ categories, setCategories ] = useState([]);
     
     // setting date range
     var curr = new Date();
@@ -49,6 +55,23 @@ const Home = ( { navigation } ) => {
     var m = curr.getMonth();
     var firstDay = new Date(y, m, 1);
     var lastDay = new Date(y, m + 1, 0);
+    var lastMonthFirstDay = new Date(y, m - 1, 1);
+    var lastMonthLastDay = new Date(y, m + 1, 0);
+
+    // setting up date range for week
+    var weekFirstDay
+    var weekLastDay;
+
+    if (curr.getDay() == 0) {
+        weekFirstDay = new Date(curr.setDate(curr.getDate() - 6));
+    } else {
+        weekFirstDay = new Date(curr.setDate(curr.getDate() - curr.getDay()))
+    }
+
+    weekLastDay = new Date(curr.setDate(weekFirstDay.getDate() + 6));
+    var lastWeekFirstDay = new Date(curr.setDate(weekFirstDay.getDate() - 7));
+    var lastWeekLastDay = new Date(curr.setDate(weekLastDay.getDate() - 7));
+
 
     // setting up listener for budget changes
     const q = query(collection(projectFireStore, 'transactions/' + user?.uid + '/userTransactions'), 
@@ -74,11 +97,95 @@ const Home = ( { navigation } ) => {
                 // format the result to 2 decimal places and have commas for thousand places
                 result = parseFloat(result.toFixed(2)).toLocaleString('en-US');
                 let cats = Object.entries(dict).map(([k, v]) => [k, parseFloat(v.toFixed(2)).toLocaleString('en-US'), (parseFloat((v / result).toFixed(2)) * 100).toLocaleString('en-US')]).
-                    map(([k, v, p]) => ({ category: k, value: v, percentage: p })).sort((a,b) => b.percentage - a.percentage);
+                    map(([k, v, p]) => ({ category: k, value: v, percentage: p })).sort((a,b) => b.percentage - a.percentage).slice(0, 3);
 
                 // update state
                 setExpense(result);
                 setCategories(cats);
+        }, (error) => {
+                console.log(error);
+                setError('could not fetch data');
+        });
+
+        return () => unsubscribe();
+    },[]);
+
+    const qLastMonth = query(collection(projectFireStore, 'transactions/' + user?.uid + '/userTransactions'), 
+    where('date', '>=', lastMonthFirstDay), 
+        where('date', '<=', lastMonthLastDay));
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(qLastMonth, (snapshot) => {
+                let result = 0;
+                snapshot.forEach(doc => {
+                    result = result + doc.data().amount;
+                })
+
+                // format the result to 2 decimal places and have commas for thousand places
+                result = parseFloat(result.toFixed(2)).toLocaleString('en-US');
+
+                // update state
+                setExpenseLastMonth(result);
+        }, (error) => {
+                console.log(error);
+                setError('could not fetch data');
+        });
+
+        return () => unsubscribe();
+    },[]);
+
+    const qW = query(collection(projectFireStore, 'transactions/' + user?.uid + '/userTransactions'), 
+        where('date', '>=', weekFirstDay), 
+            where('date', '<=', weekLastDay));
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(qW, (snapshot) => {
+                const dict = {};
+                let result = 0;
+                snapshot.forEach(doc => {
+                    result = result + doc.data().amount;
+                    let cat = doc.data().category;
+
+                    // separating amounts based on category
+                    if(Object.hasOwn(dict, cat)) {
+                        dict[cat] = dict[cat] + doc.data().amount;
+                    } else {
+                        dict[cat] = doc.data().amount;
+                    }
+                })
+
+                // format the result to 2 decimal places and have commas for thousand places
+                result = parseFloat(result.toFixed(2)).toLocaleString('en-US');
+                let cats = Object.entries(dict).map(([k, v]) => [k, parseFloat(v.toFixed(2)).toLocaleString('en-US'), (parseFloat((v / result).toFixed(2)) * 100).toLocaleString('en-US')]).
+                    map(([k, v, p]) => ({ category: k, value: v, percentage: p })).sort((a,b) => b.percentage - a.percentage).slice(0, 3);
+
+                // update state
+                setExpenseWeek(result);
+                setCategoriesWeek(cats);
+        }, (error) => {
+                console.log(error);
+                setError('could not fetch data');
+        });
+
+        return () => unsubscribe();
+    },[]);
+
+    const qLastWeek = query(collection(projectFireStore, 'transactions/' + user?.uid + '/userTransactions'), 
+    where('date', '>=', lastWeekFirstDay), 
+        where('date', '<=', lastWeekLastDay));
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(qLastWeek, (snapshot) => {
+                let result = 0;
+                snapshot.forEach(doc => {
+                    result = result + doc.data().amount;
+                })
+
+                // format the result to 2 decimal places and have commas for thousand places
+                result = parseFloat(result.toFixed(2)).toLocaleString('en-US');
+
+                // update state
+                setExpenseLastWeek(result);
         }, (error) => {
                 console.log(error);
                 setError('could not fetch data');
@@ -112,7 +219,39 @@ const Home = ( { navigation } ) => {
     }
 
     const [weekSelected, setWeekSelected] = useState(true);
-    const PieChart = (<>
+    const monthData = {
+        labels: ['Last month', 'This month'],
+        datasets: [
+            {
+                data: [expenseLastMonth, expense]
+            }
+        ]
+    }
+
+    const weekData = {
+        labels: ['Last week', 'This week'],
+        datasets: [
+            {
+                data: [expenseLastWeek, expenseWeek]
+            }
+        ]
+    }
+
+    const chartConfig = {
+        backgroundGradientFrom: '#fff',
+        backgroundGradientFromOpacity: 0,
+        backgroundGradientTo: '#fff',
+        backgroundGradientToOpacity: 0.5,
+
+        color: (opacity = 1) => `#023047`,
+        labelColor: (opacity = 1) => `#333`,
+
+        barPercentage: 2,
+        decimalPlaces: 2,
+        strokeWidth: 2,
+    }
+
+    const PieChart = (<View style={{alignItems: "center",}}>
         <View style={styles.weekMonthBar}>
             <TouchableOpacity disabled={weekSelected} onPress={() => setWeekSelected(true)} 
                 style={[styles.weekMonth, weekSelected ? styles.selected : {}]}><Text>week</Text></TouchableOpacity>
@@ -120,11 +259,13 @@ const Home = ( { navigation } ) => {
             <TouchableOpacity disabled={!weekSelected} onPress={() => setWeekSelected(false)} 
                 style={[styles.weekMonth, !weekSelected ? styles.selected : {}]}><Text>month</Text></TouchableOpacity>
         </View>
+        <View style={{marginTop: 16}}>
+            {/* <Image source={require('../../../assets/DummyChart.png')} style={{height: 200, width: 232, alignSelf: 'center'}}/> */}
+            { weekSelected && <BarChart data={weekData} height={220} width={300} chartConfig={chartConfig} showValuesOnTopOfBars={true} fromZero={true} />}
+            { !weekSelected && <BarChart data={monthData} height={220} width={300} chartConfig={chartConfig} showValuesOnTopOfBars={true} fromZero={true} />}
+        </View>
 
-        <Image source={require('../../../assets/DummyChart.png')} style={{height: 200, width: 232, alignSelf: 'center'}}/>
-
-
-    </>);
+    </View>);
 
     // Note: Needs to be declared in an earlier line
     const SampleTransaction = (<View style={styles.transactionContainer}>
@@ -170,7 +311,8 @@ const Home = ( { navigation } ) => {
             <Box content={Welcome}/>
             <TouchableOpacity><Text style={styles.report}>See full report</Text></TouchableOpacity>
             <Box content={PieChart}/>
-            <Text style={styles.transactionTitle}>Top Spendings for the month</Text>
+            { !weekSelected && <Text style={styles.transactionTitle}>Top Spendings for the month</Text> }
+            { weekSelected && <Text style={styles.transactionTitle}>Top Spendings for the week</Text> }
         </>)
     }
 
@@ -178,9 +320,12 @@ const Home = ( { navigation } ) => {
         <SafeAreaView style={styles.mainContainer}>
             <AppHeader title="SaveLah" showBell onBell={onBell}/>
 
-            {categories.length != 0 && <FlatList data={categories} keyExtractor={item => item.category} renderItem={renderTransactions} 
+            {categories.length != 0 && !weekSelected && <FlatList data={categories} keyExtractor={item => item.category} renderItem={renderTransactions} 
             ListHeaderComponent={getHeader} />}
-            
+
+            {categories.length != 0 && weekSelected && <FlatList data={categoriesWeek} keyExtractor={item => item.category} renderItem={renderTransactions} 
+            ListHeaderComponent={getHeader} />}
+
             {categories.length == 0 && <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}> 
                 <Box content={Welcome}/>
                 <TouchableOpacity><Text style={styles.report}>See full report</Text></TouchableOpacity>
